f81f0f0737d4a4d9ddba415d16768c68bd1001f1
Alex <alexallafidev.@gmail.com>
2022-12-04 10:43:57 +0200

    Day02 second

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,17 +5,22 @@ fun main() {
             val (opp, me) = it.split(" ")
             val oppM = OpponentMove.valueOf(opp)
             val meM = MyMove.valueOf(me)
-            score(oppM, meM)
+            scorePart1(oppM, meM)
         }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+
+        return input.sumOf {
+            val (opp, result) = it.split(" ")
+            val oppM = OpponentMove.valueOf(opp)
+            val resultMv = MovesResult.fromMyMove(MyMove.valueOf(result))
+            scorePart2(oppM, resultMv)
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("In02.txt")
-    print(part1(testInput))
+    print(part2(testInput))
 }
 
 enum class OpponentMove {
@@ -26,16 +31,24 @@ enum class MyMove {
     X, Y, Z
 }
 
-fun score(opponentMove: OpponentMove, myMove: MyMove): Int {
+fun scorePart1(opponentMove: OpponentMove, myMove: MyMove): Int {
 
     if (Move.fromMe(myMove) == Move.fromOpponent(opponentMove)) {
         return 3 + Move.fromMe(myMove).points
     }
 
-    return if (Move.winningPairs.contains(Pair(Move.fromMe(myMove), Move.fromOpponent(opponentMove)))) {
+    return if (Move.winsTo.contains(Pair(Move.fromMe(myMove), Move.fromOpponent(opponentMove)))) {
         6 + Move.fromMe(myMove).points
     } else {
         Move.fromMe(myMove).points
+    }
+}
+
+fun scorePart2(opponentMove: OpponentMove, expectedResult: MovesResult): Int {
+    return when(expectedResult) {
+        MovesResult.Win -> Move.losesToMap[Move.fromOpponent(opponentMove)]!!.points + 6
+        MovesResult.Lose -> Move.winsToMap[Move.fromOpponent(opponentMove)]!!.points
+        MovesResult.Draw -> Move.fromOpponent(opponentMove).points + 3
     }
 }
 
@@ -46,7 +59,15 @@ enum class Move(val points: Int) {
 
     companion object {
 
-        val winningPairs = listOf(
+
+        // ----- Part 1 -----------
+        val winsTo = listOf(
+            Paper to Rock,
+            Rock to Scissors,
+            Scissors to Paper
+        )
+
+        val winsToMap = mapOf(
             Paper to Rock,
             Rock to Scissors,
             Scissors to Paper
@@ -66,6 +87,29 @@ enum class Move(val points: Int) {
                 MyMove.Z -> Scissors
             }
         }
+
+        // ------- Part 2 --------
+
+        val losesToMap = mapOf(
+            Paper to Scissors,
+            Rock to Paper,
+            Scissors to Rock
+        )
     }
 }
+
+enum class MovesResult {
+    Win, Lose, Draw;
+
+    companion object {
+        fun fromMyMove(myMove: MyMove): MovesResult {
+            return when(myMove) {
+                MyMove.X -> Lose
+                MyMove.Y -> Draw
+                MyMove.Z -> Win
+            }
+        }
+    }
+}
+
 
