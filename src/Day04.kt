d730d9c8e64f4950150fdf107cff33ed90b118c8
Alex <alexallafidev.@gmail.com>
2022-12-06 23:07:57 +0200

    Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -10,11 +10,15 @@ fun main() {
 
     fun part2(input: List<String>): Int {
 
-        return 0
+        return input
+            .map {
+                it.split(",")
+                    .map { range -> range.split("-") }
+            }.count { it.overlaps() }
     }
 
     val testInput = readInput("In04.txt")
-    print(part1(testInput))
+    print(part2(testInput))
 }
 
 fun List<List<String>>.hasARangeIncludedInOther(): Boolean {
@@ -27,4 +31,17 @@ fun List<List<String>>.hasARangeIncludedInOther(): Boolean {
     val secondInFirst = (firstRange.first <= secondRange.first) && (firstRange.last >= secondRange.last)
 
     return firstInSecond || secondInFirst
+}
+
+fun List<List<String>>.overlaps(): Boolean {
+    val thisToInt = this.map { it.map { s -> s.toInt() } }
+
+    val firstRange = thisToInt.first().first()..thisToInt.first()[1]
+    val secondRange = thisToInt[1].first()..thisToInt[1][1]
+
+    return firstRange.overlaps(secondRange)
+}
+
+fun IntRange.overlaps(other: IntRange): Boolean {
+    return this.any { it in other }
 }
